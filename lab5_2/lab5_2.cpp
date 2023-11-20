17c06159d622a25e5f796fc37e71e399b1d657ce
Newstar12321 <tazeev.zhenya@mail.ru>
2023-11-20 11:01:25 +0700

    new

--- lab5_2/lab5_2.cpp
+++ lab5_2/lab5_2.cpp
@@ -5,7 +5,6 @@
 */
 
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
@@ -13,12 +12,12 @@ int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	const int MAX = 10;
+	const size_t MAX = 10;
 	int array[MAX];
 	size_t n;
 	cout << "Введите число элементов массива" << endl << "n = ";
 	cin >> n;
-	if ((n < 1) || (n > MAX))
+	if ((n < 2) || (n > MAX))
 	{
 		cerr << "Ошибка: размер массива не может быть меньше 1 и не должен превышать 10";
 		return 1;
@@ -33,36 +32,26 @@ int main()
 
 	int new_array[MAX];
 
+	new_array[0] = array[0];
 	if (array[0] < array[1])
 	{
-		new_array[0] = pow(array[0], 2);
-	}
-	else
-	{
-		new_array[0] = array[0];
+		new_array[0] *= new_array[0];
 	}
 
-	for (size_t i = 1; i < (n-1); i++)
+	for (size_t i = 1; i < (n - 1); i++)
 	{
 		int a0 = array[i - 1], a1 = array[i], a2 = array[i + 1];
 		new_array[i] = a1;
 		if ((a1 < a0) && (a1 < a2))
 		{
-			new_array[i] = pow(a1, 2);
-		}
-		else
-		{
-			new_array[i] = a1;
+			new_array[i] *= new_array[i];
 		}
 	}
 
-	if (array[n-1] < array[n-2])
+	new_array[n - 1] = array[n - 1];
+	if (array[n - 1] < array[n - 2])
 	{
-		new_array[n-1] = pow(array[n-1], 2);
-	}
-	else
-	{
-		new_array[n-1] = array[n-1];
+		new_array[n - 1] *= new_array[n - 1];
 	}
 
 	cout << "[" << new_array[0];
